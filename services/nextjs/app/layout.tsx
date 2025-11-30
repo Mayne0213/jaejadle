322adf9ff924e8d6fd948e42b70e605ba982e159
Mayne0213 <bluemayne0213@icloud.com>
2025-11-30 11:51:46 +0900

    CHORE(app): metadata

--- services/nextjs/app/layout.tsx
+++ services/nextjs/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/widgets/Header";
 import Footer from "@/components/widgets/Footer";
+import { metadata } from "./config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,14 +14,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "제자들교회 - 인천",
-  description: "마음속 비를 막아주는 따뜻한 마음주치의, 마음우산. 나의 잠재력을 발휘하는 건강한 일상으로 여유와 품격을 더한 올바른 진료",
-  icons: {
-    icon: '/favicon.ico',
-    apple: '/favicon.ico',
-  },
-};
+export { metadata };
 
 export default function RootLayout({
   children,
